998fa2feb118b86544f305f52a1688c6ca7b70cb
pinan <pinan@MacBook-Pro-3.local>
2017-11-24 17:53:07 +0800

    在分枝上写代码，并提交代码

--- app/src/test/java/com/example/pinan/otoutiao/ExampleUnitTest.java
+++ app/src/test/java/com/example/pinan/otoutiao/ExampleUnitTest.java
@@ -12,27 +12,6 @@ import static org.junit.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        int[] nums = new int[]{1, 7, 9, 3, 15};
-        int[] ints = twoSum(nums, 10);
-        if (ints != null) {
-            System.out.println(ints[0] + " / " + ints[1]);
-        } else {
-            System.out.println("不存在的");
-        }
         assertEquals(4, 2 + 2);
-    }
-    
-    public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    arr[0] = i;
-                    arr[1] = j;
-                    return arr;
-                }
-            }
-        }
-        return null;
     }
 }
